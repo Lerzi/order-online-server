a8fedffb5e3cb0ee4e2d04fe8fc1e472e44c7668
huanshun <569259131@qq.com>
2023-09-01 16:24:33 +0800

    feat:swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { TransformInterceptor } from './transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { knife4jSetup } from 'nest-knife4j';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  knife4jSetup(app, [
+    {
+      name: '2.X版本',
+      url: `/api-json`,
+      swaggerVersion: '2.0',
+      location: `/api-json`,
+    },
+  ]);
   await app.listen(3001);
 }
 bootstrap();
